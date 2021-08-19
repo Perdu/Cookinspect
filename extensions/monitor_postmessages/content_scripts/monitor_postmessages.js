fe25577d3c4486e4eb932cbc1a91f2aef122d14a
Célestin Matte <celestin.matte@gmail.com>
2021-08-19 10:32:36 +0200

    update to tcfv2: update extensions/monitor_postmessages

--- extensions/monitor_postmessages/content_scripts/monitor_postmessages.js
+++ extensions/monitor_postmessages/content_scripts/monitor_postmessages.js
@@ -1,14 +1,38 @@
 window.addEventListener("message", function(e) {
     var callid = "";
+    var version;
+    var debug = false;
     if (e.data.__cmpCall) {
 	callid = e.data.__cmpCall.callId;
-//    } else if (e.data.__cmpReturn) {
-//	callid = e.data.__cmpReturn.callId;
+        version = 1;
+    } else if (debug && e.data.__cmpReturn) {
+	callid = e.data.__cmpReturn.callId;
+        version = 1;
+    } else if (e.data.__tcfapiCall) {
+	callid = e.data.__tcfapiCall.callId;
+        version = 2;
+    } else if (debug && e.data.__tcfapiReturn) {
+	callid = e.data.__tcfapiReturn.callId;
+        version = 2;
     } else {
 	return;
     }
     var re = /^uCookie_/;
-    if (!re.test(callid)) {
-	console.log("sc-postMessage: " + e.origin, callid, e.source, e.data, e);
+    if (!re.test(callid) || debug) {
+        if (version == 1) {
+	    console.log("sc-postMessage: " + e.origin, callid, e.source, e.data, e);
+        } else {
+            if (debug) {
+                var callid = "";
+                if (e.data.__tcfapiCall) {
+                    callid = e.data.__tcfapiCall.callId;
+                }
+                if (e.data.__tcfapiReturn) {
+                    callid = e.data.__tcfapiReturn.callId;
+                }
+                console.log(e.data, callid);
+            }
+	    console.log("sc-postMessage_v2: " + e.origin, callid, e.source, e.data, e);
+        }
     }
 });
